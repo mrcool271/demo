53673e15b7c440c25081a265a09f9e962db86d35
cui <cui@192.168.166.126>
2014-02-08 20:02:43 +0800

    add

--- index.php
+++ index.php
@@ -3,7 +3,51 @@
  * 一些demo
  */
 
+//mpay_record 里面的data分账数据
+$str = 'a:10:{s:10:"busiTypeId";s:5:"DOOTA";s:13:"confirmAmount";s:6:"178.00";s:9:"confirmNo";i:1545735;s:10:"merchantId";s:14:"MLS_I_00000001";s:9:"orderDate";s:14:"20140107222108";s:7:"orderNo";s:12:"140107863745";s:9:"shareData";s:111:"[{"merchantCode":"MLS_D_00092039","amount":"178.00","freight":"0.00","coupon":"0.00","orderId":"140107863745"}]";s:7:"totalNo";s:12:"140107863745";s:7:"version";s:8:"20131111";s:4:"HMAC";s:32:"d36402fa06da70daa822332279c34a76";}'; 
+printInfo(unserialize($str));
+exit;
 
+$str = '[{"80797629":"/pic/_o/00/3f/6a94191ca703d83210087e6eff48_400_540.c6.jpg"},{"83508921":"/pic/_o/a0/f3/f41f66b0eb22a5274c74b3e8bb3d_450_540.c6.jpg"},{"79660369":"/pic/_o/ae/c3/e4d001a52683158b4a1d42fcac74_504_756.c6.jpg"},{"79660813":"/pic/_o/8b/1f/c58b9a12a2b936e098c2627f44cc_468_702.c6.jpg"}]';
+printInfo(json_decode($str,true));
+exit;
+$str = array('1234'=>'asdfdsaf');
+// echo array_values($str)[0];exit;
+echo array_shift($str);exit;
+printInfo(json_decode($str));
+exit;
+
+$arr = array(1234,12334,1234);
+var_dump(json_encode($arr));
+exit;
+// echo (int)date('H',time());
+// if(date('H',time())<6){
+// 	echo '<';
+// }else{
+// 	echo '>';
+// }
+// exit;
+
+// echo substr('123456',0,-1);
+// exit;
+
+
+//PHP获取当天零点的时间戳
+echo strtotime(date('Y-m-d',time()));
+echo '<br>';
+echo strtotime(date('2014-01-26 00:00:00'));
+echo '<br>';
+echo time();
+
+$tmp = array(
+		'a'=>array('a1'=>'123','a2'=>'456','a3'=>'111','a4'=>'ssd'),
+		'b'=>array('b1'=>'123','b2'=>'456','b3'=>'111','b4'=>'ssd'),
+		'c'=>array('c1'=>'123','c2'=>'456','c3'=>'111','c4'=>'ssd'),
+		'd'=>array('d1'=>'123','d2'=>'456','d3'=>'111','d4'=>'ssd'),
+);
+
+// printInfo(array_keys($tmp));
+exit;
 
 
 /**
@@ -155,6 +199,43 @@ function eg_empty_isset_is_null(){
 	var_dump(is_null($d));	//bool(false)
 	var_dump(is_null($e));	//bool(true)
 	var_dump(is_null($f));	//bool(false)
+	
+	if($a){
+		echo '1';
+	}else{
+		echo '0';
+	}
+	if($b){
+		echo '1';
+	}else{
+		echo '0';
+	}
+	if($c){
+		echo '1';
+	}else{
+		echo '0';
+	}
+	if($d){
+		echo '1';
+	}else{
+		echo '0';
+	}
+	if($e){
+		echo '1';
+	}else{
+		echo '0';
+	}
+	if($f){
+		echo '1';
+	}else{
+		echo '0';
+	}
+	if(!$gg){
+		echo '0';
+	}
+	else {
+		echo '1';
+	}
 }
 // eg_empty_isset_is_null();
 
