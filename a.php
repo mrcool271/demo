a637f0b637ae3b3f40fd74825a3dbcc96c1e5d53
cuiweifeng <149829908@qq.com>
2017-02-04 12:55:47 +0800

    test

--- a.php
+++ a.php
@@ -40,10 +40,26 @@ function http_get($url){
 	$sContent = curl_exec($oCurl);
 	$aStatus = curl_getinfo($oCurl);
 	curl_close($oCurl);
-		return $sContent;
+    return $sContent;
 }
 
 exit;
+
+function https_get() {
+    $curl = curl_init();
+    // 刚开始抓取了https://github.com,但是页面弹框，后来改用抓取支付宝首页测试
+    curl_setopt($curl, CURLOPT_URL, 'https://www.alipay.com');
+    // 设置header
+    curl_setopt($curl, CURLOPT_HEADER, 1);
+    // 设置cURL 参数，要求结果保存到字符串中还是输出到屏幕上
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    // 运行cURL，请求网页数据
+    $data = curl_exec($curl);
+    // 关闭cURL请求
+    curl_close($curl);
+    // 打印出抓取的测试数据
+    var_dump($data);
+}
 
 echo 'master';
 echo 'merge';
